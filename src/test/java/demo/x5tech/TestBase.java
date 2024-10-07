6032b1b709325ca2d0b5730a69c98547ed3dbbc0
Anastasia Sebova <se.ana@hotmail.com>
2024-10-07 22:03:23 +0300

    JDK switched to 17

--- src/test/java/demo/x5tech/TestBase.java
+++ src/test/java/demo/x5tech/TestBase.java
@@ -6,18 +6,28 @@ import demo.x5tech.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 
 public class TestBase {
     @BeforeAll
     static void setUp() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://x5-tech.ru/";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterEach
     void addAttachments() {
-        Attach.saveScreenShot("Screen");
+        Attach.addScreenShot("Screen");
+        Attach.addVideo();
     }
 
 }
--- src/test/java/demo/x5tech/helpers/Attach.java
+++ src/test/java/demo/x5tech/helpers/Attach.java
@@ -4,13 +4,37 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.codeborne.selenide.Selenide.sessionId;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class Attach {
 
     @Attachment(value = "{fileName}", type = "image/png")
-    public static byte[] saveScreenShot(String fileName) {
+    public static byte[] addScreenShot(String fileName) {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl()
+                + "' type='video/mp4'></video></body></html>";
+    }
+
+    static URL getVideoUrl() {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
+
+
+
+
