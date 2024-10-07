911e313bbdcb15f4615db8993ecfedd06815f387
Anastasia Sebova <se.ana@hotmail.com>
2024-10-07 19:55:43 +0300

    1t test added

--- src/test/java/demo/x5tech/tests/PageHeaderTest.java
+++ src/test/java/demo/x5tech/tests/PageHeaderTest.java
@@ -5,12 +5,13 @@ import demo.x5tech.pages.components.Header;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 public class PageHeaderTest extends TestBase {
 
     @Test
     void t1() {
-        open("");
+        step("Открываем главную страницу", () -> open(""));
         Header.checkHeaderMenuItems();
     }
 }
